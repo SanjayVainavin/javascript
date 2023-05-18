d57b62b35e21a3373ec78cf884688b5246e56009
SanjayVainavin <sanjay@vainavinsystech.com>
2023-05-18 14:45:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+// var name = "apple";
+// var name = "bmw";
+// console.log(name);
+
+// let name = "google";
+// name = "Facebook";
+
+// console.log(name);
+
+// const pi = 3.14;
+// pi = 2.14;
+
+// console.log(pi)
+
+// let h1 = document.getElementById("header");
+// console.log(h1)
+// console.log(h1.innerHTML = "Hello")
+// // innerHTML,innerText
+
+// let button = document.querySelectorAll(".btn");
+// console.log(button.forEach((item) => console.log(item.innerHTML)));
+
+// let day = 1;
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+
+//   case 2:
+//     console.log("monday");
+//     break;
+
+//   default:
+//     console.log("default");
+// }
+
+// do while
+// while
+// for
+
+// foreach
+// for of
+// for in
+
+// let users = [1, 2, 3, 4, 5, 6, "sdf", "rtrt"];
+
+// for (user in users) {
+//   console.log(user);
+// }
+
+// function goole() {}
+
+// const google = function () {};
+
+// let num = 123;
+// const oops = (number, lfgk) => {
+//   console.log(number);
+// };
+// oops(num);
+
+// primitve
+
+// let num1 = 123; //112233
+// let num2 = 123; //3334455
+
+// swtring,number,bollean,symbol,bigint,undefine,null
+
+let name;
+console.log(name)
+// non primitve
+
+// array ,objects
+
+// let array1 = [1,2,3,4];//112233
+// let array2 = [1,2,3,4];//112233
+
+// function num(...num) {
+//   console.log(num);
+// }
+
+// let num1 = 1 //112233
+// let num2 = 2 //112233
+// let num3 = 3 //112233
+// num(num1,num2,num3);
+// let newArray = [...array1];
+// console.log(newArray);
